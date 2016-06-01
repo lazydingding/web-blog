760fff6e2d96c416d76a9b83fa04c5df2f1d4ccd
Luping Yu <lazydingding@gmail.com>
2016-06-01 17:00:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
    cookieSecret: 'myblog',
-   url: 'mongodb://<dbuser>:<dbpassword>@ds039484.mlab.com:39484/lazydingding'
+   url: 'mongodb://dingding:ylp8811211@ds039484.mlab.com:39484/lazydingding'
 };
